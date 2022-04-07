e5c1b4a747888a20a4c1fc13a9b3f27c6587a1fb
bartosz.muszynski <muszynski.bartek@wp.pl>
2022-04-07 23:45:57 +0200

    Initial commit

---
+++ kalend1.py
@@ -0,0 +1,38 @@
+import calendar
+from calendar import monthrange
+from datetime import datetime, date
+def date_check(*args):
+    for x in args:
+        y = x.split('/')
+        if len(y) == 1 :
+            year = int(y[-1])
+            if calendar.isleap(year) == True:
+                 x = print("Rok ktory podales jest przestepny")
+            else:
+                 x = print("Rok ktory podales nie jest przestepny ")
+            return(x)
+        elif len(y) == 2:
+            year = int(y[-1])
+            month = int(y[-2])
+            if calendar.isleap(year) == True:
+                x = print("Rok ktory podales jest przestepny")
+            else:
+                x = print("Rok ktory podales nie jest przestepny ")
+            y = monthrange(year, month)
+            x1 = print("Miesiac ktory podales ma ", y[-1], "dni")
+            return (x,x1)
+        elif len(y) == 3:
+            year = int(y[-1])
+            month = int(y[-2])
+            day = int(y[0])
+            if calendar.isleap(year) == True:
+                x = print("Rok ktory podales jest przestepny")
+            else:
+                x = print("Rok ktory podales nie jest przestepny ")
+            y = monthrange(year, month)
+            x1 = print("Miesiac ktory podales ma ", y[-1], "dni")
+            day_of_year = date(year, month, day).timetuple().tm_yday
+            x3 = print("Dzien roku: ", day_of_year)
+            return (x, x1,x3)
+
+date_check("01/01/2020")
---
+++ kalend3.py
@@ -0,0 +1,39 @@
+import calendar
+from calendar import monthrange
+from datetime import datetime, date
+def date_check2():
+    with open("test.txt", "r", encoding="utf-8") as file:
+        f = file.read()
+        y = f.split('/')
+        if len(y) == 1:
+            year = int(y[-1])
+            if calendar.isleap(year) == True:
+                x = print("Rok ktory podales jest przestepny")
+            else:
+                x = print("Rok ktory podales nie jest przestepny ")
+            return (x)
+        elif len(y) == 2:
+            year = int(y[-1])
+            month = int(y[-2])
+            if calendar.isleap(year) == True:
+                x = print("Rok ktory podales jest przestepny")
+            else:
+                x = print("Rok ktory podales nie jest przestepny ")
+            y = monthrange(year, month)
+            x1 = print("Miesiac ktory podales ma ", y[-1], "dni")
+            return (x, x1)
+        elif len(y) == 3:
+            year = int(y[-1])
+            month = int(y[-2])
+            day = int(y[0])
+            if calendar.isleap(year) == True:
+                x = print("Rok ktory podales jest przestepny")
+            else:
+                x = print("Rok ktory podales nie jest przestepny ")
+            y = monthrange(year, month)
+            x1 = print("Miesiac ktory podales ma ", y[-1], "dni")
+            day_of_year = date(year, month, day).timetuple().tm_yday
+            x3 = print("Dzien roku: ", day_of_year)
+            return (x, x1, x3)
+
+date_check2()
